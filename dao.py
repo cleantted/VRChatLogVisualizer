7703e01bb56cf613475e1d2d21d901d14c7f5801
cleantted <seo.cleantted@gmail.com>
2021-02-07 18:57:25 +0900

    VRChatActivityLogのdaoを実装
    会った回数を取得する関数を追加

---
+++ dao.py
@@ -0,0 +1,40 @@
+import sqlite3
+import pandas as ps
+
+
+class VRChatActivityLogsDao:
+    __table_name = "ActivityLogs"
+    __column_name = {
+        "id": "ID", 
+        "activity_type": "ActivityType", 
+        "timestamp": "Timestamp", 
+        "notificaton_id": "NotificationID", 
+        "user_id": "UserID", 
+        "user_name": "UserName", 
+        "world_id": "WorldID", 
+        "world_name": "WorldName",
+    }
+
+    def __init__(self, db_name="./VRChatActivityLog.db"):
+        self.conn = sqlite3.connect(db_name)
+
+    def __del__(self):
+        if self.conn:
+            self.conn.close()
+
+    def meets_counts(self, limit=20):
+        df = ps.read_sql(
+            f"""
+                select {self.__column_name["user_name"]}, count(*) 
+                from {self.__table_name} 
+                where {self.__column_name["activity_type"]} = 1
+                group by {self.__column_name["user_name"]}
+                order by 2 desc
+                limit {limit}
+            """, self.conn)
+        return df
+
+
+if __name__ == "__main__":
+    dao = VRChatActivityLogsDao()
+    print(dao.meets_counts())
