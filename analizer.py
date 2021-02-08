83f06ce523581a260fbcf59ab8ae4b85b23fb794
cleantted <seo.cleantted@gmail.com>
2021-02-08 23:12:24 +0900

    joinした/された人を取得

--- analizer.py
+++ analizer.py
@@ -1,5 +1,6 @@
 import dao
 import datetime
+import operator
 
 
 def is_near_time(a_str, b_str):
@@ -34,13 +35,37 @@ def world_join_status():
         players.append((world_name, time_in, users))
     # print(players)
 
+    meet_to_me = {}
+    i_meet_who = {}
     for i, (world, time, pls) in enumerate(players):
         if world == "VRChat Home":
             continue
-        already_count = len(set(pl for pl, time_in in pls if is_near_time(time, time_in))) - 1
-        comming_count = len(set(pl for pl, _ in pls)) - already_count - 1
+        already_player = set(pl for pl, time_in in pls if is_near_time(time, time_in))
+        for pl in already_player:
+            if pl == "cleantted":
+                continue
+            i_meet_who[pl] = i_meet_who.setdefault(pl, 0) + 1
+
+        comming_player = set(pl for pl, time_in in pls if not is_near_time(time, time_in))
+        for pl in comming_player:
+            meet_to_me[pl] = meet_to_me.setdefault(pl, 0) + 1
+
+        already_count = len(already_player) - 1
+        comming_count = len(comming_player)
         assert(already_count >= 0 and comming_count >= 0)
-        print(f"[{i}] {world}: すでにいた人: {already_count}人, 入ってきた人: {comming_count}人")
+        # print(f"[{i}] {world}: すでにいた人: {already_count}人, 入ってきた人: {comming_count}人")
+
+    S1 = sorted(list(i_meet_who.items()), key=operator.itemgetter(1), reverse=True)
+    S2 = sorted(list(meet_to_me.items()), key=operator.itemgetter(1), reverse=True)
+
+    print("I meet to: ")
+    for i in range(10):
+        print(f"[{i + 1}] {S1[i][0]}: {S1[i][1]}回")
+
+    print("")
+    print("who meet to me(?): ")
+    for i in range(10):
+        print(f"[{i + 1}] {S2[i][0]}: {S2[i][1]}回")
 
 
 if __name__ == "__main__":
