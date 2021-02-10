c18bed3b2c8badf2d4a75ef2f7be006e8132c5c1
cleantted <seo.cleantted@gmail.com>
2021-02-11 00:07:58 +0900

    プレイヤー同士のjoinした/された関係をとる関数を追加

--- analizer.py
+++ analizer.py
@@ -1,5 +1,6 @@
 import dao
 import datetime
+import heapq
 import operator
 
 
@@ -21,6 +22,32 @@ def fetch_world_stay_time(dao):
     ) for i in range(len(L) - 1)]
 
     return world_data
+
+
+def complete_player_data(players):
+    buffer = {}
+    player_data = []
+    for user_name, timestamp, activity_type in players: 
+        if activity_type == 1:
+            if user_name in buffer:
+                print(f"[error] already added: {user_name} at {timestamp}")
+                continue
+
+            buffer[user_name] = timestamp
+        elif activity_type == 9:
+            if user_name not in buffer:
+                print(f"[error] not exist: {user_name} at {timestamp}")
+                continue
+            player_data.append((buffer.pop(user_name), timestamp, user_name))
+        else:
+            print(f"[error] unknown type: {activity_type} of {user_name} at {timestamp}")
+    
+    for user_name, timestamp in buffer.items():
+        print(f"[warn] not leaved: {user_name} from {timestamp}")
+        player_data.append((timestamp, "9999-99-99 23:59:59", user_name))
+    player_data.sort()
+    # print(player_data)
+    return player_data
 
 
 def fetch_players_in_world(dao, world_data):
@@ -73,8 +100,49 @@ def show_join_or_joined(players):
         print(f"[{i + 1}] {S2[i][0]}: {S2[i][1]}回")
 
 
+def pickup(d, limit):
+    L = sorted(list(d.items()), key=operator.itemgetter(1), reverse=True)[:limit]
+
+    res = ""
+    for i, (name, count) in enumerate(L):
+        res += f"{name}[{count}], "
+    return res
+
+def relation_analsys(players):
+    valid_player_data = filter(lambda x: x[2] != "9999-99-99 23:59:59", players)
+    relation = {"cleantted": dict()}
+    for i, (world, time, pls) in enumerate(valid_player_data):
+        queue = []
+        joinning_player = {"cleantted"}
+        for in_time, out_time, pl in pls:
+            if pl == "cleantted":
+                continue
+
+            while queue and queue[0][0] < in_time:
+                joinning_player.remove(heapq.heappop(queue)[1])
+            
+            if is_near_time(time, in_time):
+                relation["cleantted"][pl] = relation["cleantted"].setdefault(pl, 0) + 1
+            else:
+                for pl2 in joinning_player:
+                    if pl2 == "cleantted":
+                        continue
+                    relation[pl][pl2] = relation.setdefault(pl, dict()).setdefault(pl2, 0) + 1
+
+            heapq.heappush(queue, (out_time, pl))
+            joinning_player.add(pl)
+    
+    for pl, to_join_list in relation.items():
+        if max(to_join_list.values()) < 2:
+            continue
+        print(f"{pl}: {pickup(to_join_list, 5)}")
+    # print(relation)
+    return relation
+
+
 if __name__ == "__main__":
     activity_logs_dao = dao.VRChatActivityLogsDao()
     world_data = fetch_world_stay_time(activity_logs_dao)
     players = fetch_players_in_world(activity_logs_dao, world_data)
-    show_join_or_joined(players)
+    # show_join_or_joined(players)
+    relation = relation_analsys(players)
